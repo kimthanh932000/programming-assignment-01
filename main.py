376dd1787a2f6692fc69191fe129247a888b026d
Kim Thanh Tran <kimthanh932000@gmail.com>
2024-03-16 23:17:31 +0800

    refactor main.py
    update pseudocode

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ def random_list(quantity, minimum, maximum):
     result = []
     
     for i in range(quantity):
-        num = random.randint(minimum, maximum)
-        result.append(num)
+        number = random.randint(minimum, maximum)
+        result.append(number)
 
     return result
 
@@ -17,13 +17,13 @@ def get_question(number, randomList):
 
     if number == 1:
         question = "What is the smallest number in this list? " + str(randomList)
-        
+
     elif number == 2:
         question = "What is the biggest number in this list? " + str(randomList)
-        
+
     elif number == 3:
         question = "What is the sum of the numbers in this list? " + str(randomList)
-        
+
     else:
         question = "What is the average of the numbers in this list? " + str(randomList) + "(round UP to nearest integer)"
 
@@ -48,55 +48,66 @@ def get_correct_answer(number, randomList):
     return answer
 
 ##################################
+def is_valid(value):
+    if value.upper() in ["E", "M", "H"]:
+        return True
+
+    return False
+
+###################################
+def get_difficulty_attributes(difficulty):
+    if difficulty == "E":
+        return [2, 3, 1, 5]
+    
+    elif difficulty == "M":
+        return [4, 5, 3, 12]
+    
+    return [6, 8, 10, 25]
+
+##################################
+def print_selcted_difficulty(difficulty):
+    text = ""
+
+    if difficulty == "E":
+        text = "Easy"
+        
+    elif difficulty == "M":
+        text = "Medium"
+        
+    else:
+        text = "Hard"
+
+    print text + " difficulty selected!\n"
+
+###############################
 
 print("Welcome to the Number List Test program.")
 
-difficulty = input("Select a difficulty:\n[E]asy\n[M]edium\n[H]ard\n").upper()
+difficulty = input("Select a difficulty:\n[E]asy\n[M]edium\n[H]ard\n")
 
 while True:
-    if difficulty in ['E', 'M', 'H']:
+    if is_valid(difficulty):
+        print_selcted_difficulty(difficulty)
         break
-    difficulty = input("Invalid choice! Enter E, M or H.\n").upper()
-
-if difficulty == 'E':
-    print("Easy difficulty selected!\n")
-elif difficulty == 'M':
-    print("Medium difficulty selected!\n")
-else:
-    print("Hard difficulty selected!\n")
-
-questions = None
-quantity = None
-minimum = None
-maximum = None
-
-if difficulty == 'E':
-    questions = 2
-    quantity = 3
-    minimum = 1
-    maximum = 5
     
-elif difficulty == 'M':
-    questions = 4
-    quantity = 5
-    minimum = 3
-    maximum = 12
+    difficulty = input("Invalid choice! Enter E, M or H.\n")
 
-else:
-    questions = 6
-    quantity = 8
-    minimum = 10
-    maximum = 25
+attributes = get_difficulty_attributes(difficulty)
+
+questions = attributes[0]
+quantity = attributes[1]
+minimum = attributes[2]
+maximum = attributes[3]
 
 score = 0
-randomList = None
 
-for question in range(questions):
-    print("Question" + str(question + 1) + " of " + str(questions) + ".")
+for i in range(questions):
+    randomList = None
+
+    print("Question" + str(i + 1) + " of " + str(questions) + ".")
 
     if question + 1 < questions:
         randomList = random_list(quantity, minimum, maximum)
-
     else:
         print("Challenge questions!")
         randomList = random_list(quantity, minimum * 2, maximum * 2)
@@ -104,7 +115,9 @@ for question in range(questions):
     number = random.randint(1,4)
 
     print(get_question(number, randomList))
+    
     correctAnswer = get_correct_answer(number, randomList)
+    
     answer = int(input())
 
     if answer == correctAnswer:
@@ -117,16 +130,6 @@ print("Test complete!\nYou scored", str(score) + "/" + str(questions), "(" + str
 
 if score == questions:
     print("Perfect score, well done!")
-
-
-
-
-
-
-
-    
-
-
 
 
 
